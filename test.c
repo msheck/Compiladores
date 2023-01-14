7e6796f7170b5d60f0e98166e33815840463c427
ViniciusMeirelles <vinicius.meirelles@outlook.com.br>
2023-01-13 22:20:40 -0300

    Added if-then-else tests

--- test.c
+++ test.c
@@ -32,7 +32,7 @@ int test_parse (char* string, int assertion);
 int main (int argc, char **argv)
 {
 	int test_result = true;
-	error_message_enabled = 0;
+	error_message_enabled = 1;
 
     // EMPTY PROGRAM
 	test_result = test_result && test_parse("", true);
@@ -70,6 +70,18 @@ int main (int argc, char **argv)
         "}"
         , true);
     
+    // IF-THEN-ELSE
+	test_result = test_result && test_parse("if (a) then{}", false);
+	test_result = test_result && test_parse("int commBlock (){if () then{}}", false);
+	test_result = test_result && test_parse("int commBlock (){if (!!(a+b*c)) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if (b*(!c)) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if ((a+d)+b+c) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if((!!!--((----a)))>(!!!!(a))) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if(b[a^a]) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if(abx()*abx()) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if(abx(aaa)) then{}}", true);
+	test_result = test_result && test_parse("int commBlock (){if((banana[!fff()^ffr(abc)*foo(a, b, d, a+x+50*37*4)] + frr(32)) > 100) then {}}", true);
+	
     if (!test_result)
         printf("\n\033[0;31mSOME TESTS FAILED!\033[0m\n\n");
     else
@@ -136,11 +148,11 @@ int test_token(char* string, int expected_token, bool assertion) {
 
 
 int test_parse (char* string, int assertion) {
+    printf("\n-----------input start-----------\n%s\n------------input end------------\nResult: ", string);
     YY_BUFFER_STATE buffer = yy_scan_string(string);
     int ret = yyparse();
     yy_delete_buffer(buffer);
     yylex_destroy();
-    printf("\n-----------input start-----------\n%s\n------------input end------------\nResult: ", string);
     if (ret==1) { // Failure
         if(assertion) { // Success expected
             printf("\033[0;31mFAILED!\033[0m\n");
