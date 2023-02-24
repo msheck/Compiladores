9e25d2bb153b6ddad20d99a1b94aef922ca5395b
Mateus Heck <mateussheck@gmail.com>
2023-02-24 18:36:24 -0300

    Working lists

--- Tabela.c
+++ Tabela.c
@@ -7,7 +7,107 @@
 // ---------------------------- MAIN ----------------------------
 
 int main(){
+    /*INT LIST*/
+    IntList *list_ints = intList_new();
+    list_ints = intList_pushLeft(list_ints, 667);
+    list_ints = intList_pushLeft(list_ints, 420);
+    list_ints = intList_pushLeft(list_ints, 69);
+    list_ints = intList_pushLeft(list_ints, 505);
+    list_ints = intList_pushLeft(list_ints, 137);
+    printf("\n\n--------------\tINT LIST PUSHING COMPLETE\t--------------\n");
 
+    intList_print(list_ints);
+
+    intList_free(list_ints);
+    printf("\n\n--------------\tINT LIST FREEING COMPLETE\t--------------\n");
+
+    /*CONTENT*/
+    lexValue lex_val1;
+    lex_val1.line_number = 3;
+    lex_val1.type = TYPE_LIT;
+    lex_val1.value = strdup("conteudo1");
+
+    lexValue lex_val2;
+    lex_val2.line_number = 3;
+    lex_val2.type = TYPE_LIT;
+    lex_val2.value = strdup("conteudo2");
+
+    lexValue lex_val3;
+    lex_val3.line_number = 3;
+    lex_val3.type = TYPE_LIT;
+    lex_val3.value = strdup("conteudo3");
+
+    lexValue lex_val4;
+    lex_val4.line_number = 3;
+    lex_val4.type = TYPE_LIT;
+    lex_val4.value = strdup("conteudo4");
+
+    lexValue lex_arg1;
+    lex_arg1.line_number = 3;
+    lex_arg1.type = TYPE_LIT;
+    lex_arg1.value = strdup("arg1");
+
+    lexValue lex_arg2;
+    lex_arg2.line_number = 3;
+    lex_arg2.type = TYPE_LIT;
+    lex_arg2.value = strdup("arg2");
+
+    lexValue lex_arg3;
+    lex_arg3.line_number = 3;
+    lex_arg3.type = TYPE_LIT;
+    lex_arg3.value = strdup("arg3");
+
+    lexValue lex_arg4;
+    lex_arg4.line_number = 3;
+    lex_arg4.type = TYPE_LIT;
+    lex_arg4.value = strdup("arg4");
+    printf("\n\n--------------\tLEXVALS CREATED\t--------------\n");
+
+    int **dimensions = malloc(sizeof(int*) * 3);
+    int dim1 = 1;
+    int dim2 = 2;
+    int dim3 = 3;
+    dimensions[0] = &dim1;
+    dimensions[1] = &dim2;
+    dimensions[2] = &dim3;
+    printf("\n\n--------------\tDIMENSIONS CREATED\t--------------\n");
+
+    Content *arg1 = content_new(lex_arg1, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content *arg2 = content_new(lex_arg2, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content *arg3 = content_new(lex_arg3, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content *arg4 = content_new(lex_arg4, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content** args1 = malloc(sizeof(Content*)*2);
+    args1[0] = arg1;
+    args1[1] = arg2;
+    Content** args2 = malloc(sizeof(Content*)*2);
+    args2[0] = arg3;
+    args2[1] = arg4;
+    printf("\n\n--------------\tARGUMENTS CREATED\t--------------\n");
+
+    Content *cont1 = content_new(lex_val1, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content *cont2 = content_new(lex_val2, NAT_LIT, LIT_TYPE_INT, NULL, 0, NULL, 0);
+    Content *cont3 = content_new(lex_val3, NAT_LIT, LIT_TYPE_INT, dimensions, 3, args1, 2);
+    Content *cont4 = content_new(lex_val4, NAT_LIT, LIT_TYPE_INT, NULL, 0, args2, 2);
+    printf("\n\n--------------\tCONTENT NEW COMPLETE\t--------------\n");
+
+    /*CONTENT LIST*/
+    ContentList *list_cont = contentList_new();
+    list_cont = contentList_pushLeft(list_cont, cont1);
+    list_cont = contentList_pushLeft(list_cont, cont2);
+    list_cont = contentList_pushLeft(list_cont, cont3);
+    list_cont = contentList_pushLeft(list_cont, cont4);
+    printf("\n\n--------------\tCONTENT LIST PUSHING COMPLETE\t--------------\n");
+
+    contentList_print(list_cont);
+
+    contentList_free(list_cont);
+    printf("\n\n--------------\tCONTENT LIST FREEING COMPLETE\t--------------\n");
+
+    // intList_free(list_ints);
+    // printf("\n\n--------------\tINT LIST FREEING COMPLETE\t--------------\n");
+
+
+/*
     IntList *intList = intList_new();
     printf("\nPUSHING");
     intList = intList_pushLeft(intList, 1);
@@ -95,6 +195,8 @@ int main(){
 
     //table_free(table);
     //printf("\nFREE DONE!!!\n");
+*/
+    printf("\n\n\n");
     return 0;
 }
 
@@ -203,14 +305,14 @@ Content* content_new(lexValue lex_val, int nat, int lit_type, int** dimensions, 
 void content_free(Content *content){
     if (content->dimensions != NULL){
         free(content->dimensions);
+        printf("\n\tDimensions freed.");
     }
-    printf("\nDIMENSIONS FREED");
     if (content->args != NULL){
-        Content *arg = content->args[0];
-        for(int i=0; arg != NULL; i++){
-            content_free(arg);
-            arg = content->args[i];
+        for(int i=0; i < content->args_size; i++){
+            content_free(content->args[i]);
         }
+        free(content->args);
+        printf("\n\tArguments freed.");
     }
     free(content->lex_value.value);
     free(content);
@@ -222,21 +324,28 @@ IntList* intList_new() {
     return NULL;
 }
 
-IntList*  intList_pushLeft(IntList* list, int value) {
+IntList* intList_pushLeft(IntList* list, int value) {
     IntList* new_node;
-    new_node = malloc(sizeof(IntList*));
-    if(list==NULL)
+    if(list==NULL){
+        new_node = malloc(sizeof(IntList*)+sizeof(NULL));
         new_node->next = NULL;
-    else
+    }
+    else{
+        new_node = malloc(sizeof(IntList*)+sizeof(list));
         new_node->next = list;
+    }
     new_node->value = value;
+    printf("\n%d: Pushing %ld before %ld", new_node->value, (long)new_node, (long)new_node->next);
     return new_node;
 }
 
 void intList_free(IntList* list){
-    if(list->next != NULL)
-        intList_free(list->next);
-    free(list);
+    if(list != NULL){
+        if(list->next != NULL)
+            intList_free(list->next);
+        printf("\nFreeing %d: %ld...", list->value, (long)list );
+        free(list);
+    }    
 }
 
 void _intList_print(IntList* list){
@@ -264,20 +373,27 @@ ContentList* contentList_new() {
 
 ContentList*  contentList_pushLeft(ContentList* list, Content* value) {
     ContentList* new_node;
-    new_node = malloc(sizeof(ContentList*));
-    if(list==NULL)
+    if(list==NULL){
+        new_node = malloc(sizeof(ContentList*)+sizeof(NULL));
         new_node->next = NULL;
-    else
+    }
+    else{
+        new_node = malloc(sizeof(ContentList*)+sizeof(list));
         new_node->next = list;
+    }
     new_node->value = value;
+    printf("\n%s: Pushing %ld before %ld", new_node->value->lex_value.value, (long)new_node, (long)new_node->next);
     return new_node;
 }
 
 void contentList_free(ContentList* list){
-    if(list->next != NULL)
-        contentList_free(list->next);
-    content_free(list->value);
-    free(list);
+    if(list != NULL){
+        if(list->next != NULL)
+            contentList_free(list->next);
+        printf("\nFreeing %s: %ld...", list->value->lex_value.value, (long)list );
+        content_free(list->value);
+        free(list);
+    }
 }
 
 void _contentList_print(ContentList* list){
