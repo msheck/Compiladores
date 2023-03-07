d7d1afd954884a6e9be6aba65b24b397629431f0
Mateus Heck <mateussheck@gmail.com>
2023-03-07 12:43:40 -0300

    fix to expr

--- src/ASTExpressions.c
+++ src/ASTExpressions.c
@@ -20,6 +20,12 @@ char* resolve_unary_expr(char operator, ASTree *expr, int node_type){
         case 1: //bool
         case 3: //int
             expr_intValue = atoi(expr->data.value);
+            if((expr_intValue == 0)&&(expr->data.value[0] != '0')){
+                buffer = malloc(sizeof(char*)*2);
+                buffer[0] = operator;
+                buffer[1] = '\0';
+                buffer = strcat(buffer, expr->data.value);
+            }
             break;
         case 4: //float
             expr_floatValue = atof(expr->data.value);
@@ -66,18 +72,46 @@ char* resolve_binary_expr(ASTree *expr1, char* operator, ASTree *expr2, int node
     int expr1_intValue, expr2_intValue;
     float expr1_floatValue, expr2_floatValue;
     char expr1_charValue, expr2_charValue;
-    char* buffer;
+    char* buffer = NULL;
     int value_i;
     float value_f;
     switch (node_type){
         case 1: //bool
         case 3: //int
             expr1_intValue = atoi(expr1->data.value);
+            if((expr1_intValue == 0)&&(expr1->data.value[0] != '0')){
+                buffer = strdup(expr1->data.value);
+                buffer = strcat(buffer, operator);
+            }
             expr2_intValue = atoi(expr2->data.value);
+            if(buffer != NULL){
+                buffer = strcat(buffer, expr2->data.value);
+                return buffer;
+            }
+            if((expr2_intValue == 0)&&(expr2->data.value[0] != '0')){
+                buffer = strdup(expr2->data.value);
+                buffer = strcat(buffer, operator);
+                buffer = strcat(buffer, expr1->data.value);
+                return buffer;
+            }
             break;
         case 4: //float
             expr1_floatValue = atof(expr1->data.value);
+            if((expr1_floatValue == 0)&&(expr1->data.value[0] != '0')){
+                buffer = strdup(expr1->data.value);
+                buffer = strcat(buffer, operator);
+            }
             expr2_floatValue = atof(expr2->data.value);
+            if(buffer != NULL){
+                buffer = strcat(buffer, expr2->data.value);
+                return buffer;
+            }
+            if((expr2_floatValue == 0)&&(expr2->data.value[0] != '0')){
+                buffer = strdup(expr2->data.value);
+                buffer = strcat(buffer, operator);
+                buffer = strcat(buffer, expr1->data.value);
+                return buffer;
+            }
             break;
         case 2: // char
             expr1_charValue = expr1->data.value[0];
@@ -334,7 +368,6 @@ char* resolve_binary_expr(ASTree *expr1, char* operator, ASTree *expr2, int node
 
 ASTree* ast_expr_node(ASTree *expr1, lexValue operator, ASTree *expr2){
     int node_type = expr2->node_type;
-    // int isLit = false;
     char* expr_result;
     if(expr1->data.label != NULL){
         if(expr1->node_type != expr2->node_type){
@@ -361,44 +394,22 @@ ASTree* ast_expr_node(ASTree *expr1, lexValue operator, ASTree *expr2){
             }
         }
         if((expr1->data.value == NULL)||(expr2->data.value == NULL)){
-            expr_result = strdup("");
-            if(expr1->data.type != 4)
-                expr_result = strcat(expr_result, expr1->data.label);
-            else
-                expr_result = strcat(expr_result, expr1->data.value);
-            expr_result = strcat(expr_result, operator.label);
-            if(expr2->data.type != 4)
-                expr_result = strcat(expr_result, expr2->data.label);
-            else
+            if((expr1->data.value == NULL)&&(expr2->data.value != NULL)){
+                expr_result = strdup(expr1->data.label);
+                expr_result = strcat(expr_result, operator.label);
                 expr_result = strcat(expr_result, expr2->data.value);
+            }
+            else if((expr1->data.value != NULL)&&(expr2->data.value == NULL)){
+                expr_result = strdup(expr2->data.label);
+                expr_result = strcat(expr_result, operator.label);
+                expr_result = strcat(expr_result, expr1->data.value);
+            }
+            else{
+                expr_result = strdup(expr1->data.label);
+                expr_result = strcat(expr_result, operator.label);
+                expr_result = strcat(expr_result, expr2->data.label);
+            }
         }
-        else if((expr1->data.type != 4)||(expr2->data.type != 4)){
-            expr_result = strdup(expr1->data.value); 
-            expr_result = strcat(expr_result, operator.label); 
-            expr_result = strcat(expr_result, expr2->data.value);
-        }
-        //     if((expr1->data.value == NULL)&&(expr2->data.value == NULL)){
-        //         expr_result = strdup(expr1->data.label); 
-        //         expr_result = strcat(expr_result, operator.label); 
-        //         expr_result = strcat(expr_result, expr2->data.label); 
-        //     }
-        //     else if(expr1->data.value == NULL){
-        //         expr_result = strdup(expr1->data.label); 
-        //         expr_result = strcat(expr_result, operator.label); 
-        //         expr_result = strcat(expr_result, expr2->data.value); 
-        //     }
-        //     else if(expr2->data.value == NULL){
-        //         expr_result = strdup(expr1->data.value); 
-        //         expr_result = strcat(expr_result, operator.label); 
-        //         expr_result = strcat(expr_result, expr2->data.label); 
-        //     }
-        //     else{
-        //         expr_result = strdup(expr1->data.value); 
-        //         expr_result = strcat(expr_result, operator.label); 
-        //         expr_result = strcat(expr_result, expr2->data.value); 
-        //         isLit = true;
-        //     }            
-        //}
         else
             expr_result = resolve_binary_expr(expr1, operator.label, expr2, node_type);
     }
@@ -407,16 +418,6 @@ ASTree* ast_expr_node(ASTree *expr1, lexValue operator, ASTree *expr2){
             expr_result = strdup(operator.label);
             expr_result = strcat(expr_result, expr2->data.label);
         }
-        // if(expr2->data.type != 4){
-        //     if(expr2->data.value == NULL){
-        //         expr_result = strdup(operator.label); 
-        //         expr_result = strcat(expr_result, expr2->data.label); 
-        //     }
-        //     else{
-        //         expr_result = strdup(operator.value); 
-        //         expr_result = strcat(expr_result, expr2->data.value); 
-        //     }
-        // }
         else
             expr_result = resolve_unary_expr(operator.label[0], expr2, node_type);
     }
